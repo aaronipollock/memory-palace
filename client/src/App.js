22a06a07a42213de09f6e10f019c73a5c30861a0
Aaron <aaronipollock@gmail.com>
2025-09-06 15:50:01 -0700

    Switch to HashRouter for better Render compatibility

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import InputPage from './components/InputPage';
 import VisualizerPage from './components/VisualizerPage';
 import SavedRooms from './components/SavedRooms';
